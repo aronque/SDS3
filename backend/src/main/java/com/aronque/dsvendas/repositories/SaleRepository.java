7ac654b223ec6473c89aeeba7fbe231eea54d3c9
aronque <vitorsiwerskiaronque@gmail.com>
2021-05-05 18:16:54 -0300

    Group by search

--- backend/src/main/java/com/aronque/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/aronque/dsvendas/repositories/SaleRepository.java
@@ -1,7 +1,20 @@
 package com.aronque.dsvendas.repositories;
 
+import com.aronque.dsvendas.dto.SaleSumDTO;
+import com.aronque.dsvendas.dto.SaleSuccessDTO;
 import com.aronque.dsvendas.entities.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("SELECT new com.aronque.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) " +
+            " FROM Sale AS obj GROUP BY obj.seller")
+    List<SaleSumDTO> amountGroupedBySeller();
+
+    @Query("SELECT new com.aronque.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) " +
+            " FROM Sale AS obj GROUP BY obj.seller")
+    List<SaleSuccessDTO> successGroupedBySeller();
 }
--- backend/src/main/java/com/aronque/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/aronque/dsvendas/services/SaleService.java
@@ -1,6 +1,8 @@
 package com.aronque.dsvendas.services;
 
 import com.aronque.dsvendas.dto.SaleDTO;
+import com.aronque.dsvendas.dto.SaleSuccessDTO;
+import com.aronque.dsvendas.dto.SaleSumDTO;
 import com.aronque.dsvendas.entities.Sale;
 import com.aronque.dsvendas.repositories.SaleRepository;
 import com.aronque.dsvendas.repositories.SellerRepository;
@@ -9,10 +11,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class SaleService {
@@ -28,5 +28,15 @@ public class SaleService {
         sellerRepository.findAll();
         Page<Sale> result = repository.findAll(pageable);
         return result.map(x -> new SaleDTO(x));
+    }
+
+    @Transactional
+    public List<SaleSumDTO> amountGroupedBySeller() {
+        return repository.amountGroupedBySeller();
+    }
+
+    @Transactional
+    public List<SaleSuccessDTO> successGroupedBySeller() {
+        return repository.successGroupedBySeller();
     }
 }
